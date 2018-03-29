50f216a8b27132a7dcba1c5ce873c5cb20759f8c
Vladimir Popov <v.popov9@g.nsu.ru>
2018-03-29 16:46:10 +0700

    initial

---
+++ src/main/java/bolodya/mcdu/McduApplication.java
@@ -0,0 +1,12 @@
+package bolodya.mcdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McduApplication.class, args);
+	}
+}
